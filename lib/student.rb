b269df89fc5fcd724d890f9f2531369d50d7595e
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-09-23 15:55:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,12 +10,13 @@ def initialize(name, grade, id=nil)
 end 
 
 def self.create_table 
-  sql = <<-SQL 
+  sql =<<-SQL 
   CREATE TABLE IF NOT EXISTS students (
     id INTEGER PRIMARY KEY,
     name TEXT,
     grade TEXT)
     SQL
+    
     DB[:conn].execute(sql)
   end 
 end 
