166885c43655cbd231edbc343023419e83ebef2a
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-09-23 15:48:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,6 +9,15 @@ def initialize(name, grade, id=nil)
   @id = id 
 end 
 
+def self.create_table 
+  sql = <<- SQL 
+  "CREATE TABLE IF NOT EXISTS students (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    grade INTEGER)"; 
+    DB[:conn].execute(sql)
+  end 
+
 
  
  
